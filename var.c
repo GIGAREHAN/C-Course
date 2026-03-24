4a956d68130d6c9f3160153ffff88134412aeaf4
Rehan Adatiya <105909757+GIGAREHAN@users.noreply.github.com>
2026-03-24 21:54:40 +0530

    Add var.c with variable declarations and print statement

---
+++ var.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int num = 100;
+    float percentage = 99.5;
+    char grade = 'A';
+    double price = 1234.5678;
+    
+    printf("Int: %d, Float: %.2f, Char: %c, Double: %.4lf", num, percentage, grade, price);
+    return 0;
+}
